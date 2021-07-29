1c86d48c50f4d5f4bd486b4ddb7f1190b9bc4202
samuelarbibe <samuel.arbibe@gmail.com>
2021-07-29 13:08:54 +0300

    Add fade to bottom navbar

--- src/components/BottomNavbarHoc.js
+++ src/components/BottomNavbarHoc.js
@@ -1,4 +1,5 @@
 import { Box, Center, Container } from '@chakra-ui/layout'
+import { Fade } from '@chakra-ui/react'
 import React, { useRef } from 'react'
 import useOnScreen from '../hooks/useOnScreen'
 
@@ -8,16 +9,17 @@ const BottomNavbarHoc = ({ children }) => {
 
   return (
     <>
-      <Box ref={ref}>
+      <Box ref={ref} visibility={isVisible ? 'visible' : 'hidden'}>
         {children}
       </Box>
       {
-        !isVisible &&
-        <Center position='fixed' bottom='0' left='0' width='100%' borderTopWidth='1px' zIndex='999' p='4' bg='white'>
-          <Container maxWidth='container.lg' px='0'>
-            {children}
-          </Container>
-        </Center>
+        <Fade in={!isVisible} >
+          <Center position='fixed' bottom='0' left='0' width='100%' borderTopWidth='1px' zIndex='999' p='4' bg='white'>
+            <Container maxWidth='container.lg' px='0'>
+              {children}
+            </Container>
+          </Center>
+        </Fade>
       }
     </>
   )
