6256eb42d69b6f4e425e8ac6d89079370d1eacc3
samuelarbibe <samuel.arbibe@gmail.com>
2021-08-16 13:38:03 +0300

    Read order id from success redirect

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import queryString from 'query-string'
+import { useLocation } from 'react-router-dom'
 
 import { Text } from '@chakra-ui/react'
 import { Center, Heading } from '@chakra-ui/layout'
 
 const ThankYou = () => {
+  const { search } = useLocation()
+  const { cField1: orderId, response } = queryString.parse(search)
+
   return (
-    <Center dir='rtl' mt='36' borderRadius='md' bg='green.300' p='8' color='white' flexDir='column'>
-      <Heading>תודה על הקנייה!</Heading>
-      <Text color='white' mt='3'>פרטי ההזמנה נשלחו למייל שלכם</Text>
-    </Center>
+    !response || response === 'success'
+      ? (
+        <Center dir='rtl' mt='36' borderRadius='md' bg='green.300' p='8' color='white' flexDir='column'>
+          <Heading>תודה על הקנייה!</Heading>
+          <Text color='white' mt='3'>{orderId && `מספר הזמנה: ${orderId}`}</Text>
+          <Text color='white' mt='3'>פרטי ההזמנה נשלחו למייל שלכם</Text>
+        </Center>
+      ) : (
+        <Center dir='rtl' mt='36' borderRadius='md' bg='red.400' p='8' color='white' flexDir='column'>
+          <Heading>הייתה בעיה ביצירת ההזמנה</Heading>
+          <Text color='white' mt='3'>{orderId && `מספר הזמנה: ${orderId}`}</Text>
+          <Text color='white' mt='3'>יש ליצור קשר עימנו ולציין את מספר ההזמנה</Text>
+        </Center>
+      )
   )
 }
 
